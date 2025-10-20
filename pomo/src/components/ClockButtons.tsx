b8fd09054b8b34afafd913a642e90cf0b3ff402c
kennerskks <149176535+kennerskk@users.noreply.github.com>
2025-10-20 17:01:57 +0700

    delete never used variable for deploy on vercel

--- pomo/src/components/ClockButtons.tsx
+++ pomo/src/components/ClockButtons.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 type ClockButtonsProps = {
   isRunning: boolean;
--- pomo/src/components/ClockDisplay.tsx
+++ pomo/src/components/ClockDisplay.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 type ClockDisplayProps = {
   phase: 'learning' | 'mini' | 'full';
